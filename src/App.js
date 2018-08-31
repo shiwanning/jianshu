da2c0f00a21d92116f287a285c5aa6f58386075f
richard.n <shiwanning@aliyun.com>
2018-08-31 19:09:46 +0800

    route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Header from './common/header';
 import store from './store';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 
@@ -9,7 +10,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact render = { () => (<div>home</div>) }></Route>
+              <Route path="/detail"  exact render = { () => (<div>detail</div>) }></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
